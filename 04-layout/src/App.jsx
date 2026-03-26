f891ebd828f3aae1215734a795b80e12833ab192
Luan481 <henrriqueluan973@gmail.com>
2026-03-25 22:17:42 -0300

    layouts

---
+++ 04-layout/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return(
+  <div className="cont-app">
+    <header className="cont-header">nome/logo/links</header>
+    <main className="cont-main">Nosotros mataremos la cucarcha</main>
+    <section>aleatoria</section>
+    <footer>rodape</footer>
+  </div>
+  )
+}
+
+export default App;
